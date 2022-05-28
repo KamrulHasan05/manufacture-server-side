3b572824efabdd326d5a9e8fcddba1eeca49b48f
Kamrul Hasan <mdkamrulhasan.du@gmail.com>
2022-05-29 00:26:02 +0600

    delete route check

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config();
-app.use(cors())
-app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+app.use(cors());
+app.use(express.json());
+
+
 const uri = `mongodb+srv://${process.env.DB}:${process.env.PASSWORD}@cluster0.eivzb.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -74,7 +76,7 @@ async function run() {
             res.send(result);
         })
         // parts delete by id
-        app.delete('/delete-parts/:id', async (req, res) => {
+        app.delete('/deleteParts/:id', async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const result = await partsCollection.deleteOne(filter);
